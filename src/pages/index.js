dae176d25607646f1ae721b39a96b2b5019f71ea
Yu1ch <yu1ch@protonmail.com>
2023-05-25 15:48:23 +0100

    Gatsby tutorial p.3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,17 @@
 import * as React from 'react'
 // Import layout prop component
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Define the component
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>Testing out the Layout component from Gatsby</p>
+      <StaticImage
+        alt="A magestic duck flapping it's wings."
+        src="../images/duck.jpg"
+      />
     </Layout>
   )
 }
