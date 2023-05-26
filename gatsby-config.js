c3f515ac9299ef4d0dcdd8122b46e8ab5632f7fa
Yu1ch <yu1ch@protonmail.com>
2023-05-26 19:48:33 +0100

    Gatsby tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
         name: `blog`,
         path: `${__dirname}/blog`,
       }
-    }
+    },
+    "gatsby-plugin-mdx",
   ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,27 +6,32 @@ import {graphql} from 'gatsby'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-        <ul>
-            {
-                data.allFile.nodes.map(node => (
-                    <li key={node.name}>
-                        {node.name}
-                    </li>
-                ))
-            }
-        </ul>
+        {
+            data.allMdx.nodes.map((node) => (
+                <article key={node.id}>
+                    <h2>{node.frontmatter.title}</h2>
+                    <p>Posted: {node.frontmatter.date}</p>
+                    <p>{node.excerpt}</p>
+                </article>
+            ))
+        }
     </Layout>
   )
 }
 
 export const query = graphql`
-    query {
-        allFile {
-            nodes {
-                name
-            }
-        }
+query MyQuery {
+  allMdx(sort: {frontmatter: {date: DESC}}) {
+    nodes {
+      frontmatter {
+        date(formatString: "MMMM D, YYYY")
+        title
+      }
+      id
+      excerpt
     }
+  }
+}
 `
 
 export const Head = () => <Seo title="My Blog Posts" />
